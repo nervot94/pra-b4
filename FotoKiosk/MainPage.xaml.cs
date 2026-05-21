7853eca8a79f141ee7985781aec4a4ab13e54312
nervot94 <236539115+nervot94@users.noreply.github.com>
2026-05-21 09:29:53 +0200

    Rename "FotoKisk" to "FotoKiosk"

--- FotoKiosk/MainPage.xaml.cs
+++ FotoKiosk/MainPage.xaml.cs
@@ -3,7 +3,7 @@ using Microsoft.UI.Xaml.Controls;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace FotoKisk;
+namespace FotoKiosk;
 
 /// <summary>
 /// The main content page displayed inside the application window.
