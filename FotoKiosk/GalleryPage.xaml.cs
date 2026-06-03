06207cbdca25cb116d74b7cfa600727271fedf2a
nervot94 <236539115+nervot94@users.noreply.github.com>
2026-06-03 21:54:36 +0200

    Implement gallery page

--- FotoKiosk/GalleryPage.xaml.cs
+++ FotoKiosk/GalleryPage.xaml.cs
@@ -1,3 +1,5 @@
+using FotoKiosk.Models;
+using FotoKiosk.Services;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +14,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,8 +26,31 @@ namespace FotoKiosk;
 /// </summary>
 public sealed partial class GalleryPage : Page
 {
+    public ObservableCollection<RollercoasterPhoto> Images { get; } = [];
+    private LocalPhotoService PhotoService { get; } = new();
+
     public GalleryPage()
     {
+        DataContext = this;
         InitializeComponent();
+
+        _ = GetRollercoasterPhotos();
+    }
+    
+    private async Task GetRollercoasterPhotos()
+    {
+        var photosDirectory = PhotoService.GetDirectory();
+        var photos = await PhotoService.GetPhotosAsync(photosDirectory);
+        var groupedPhotos = await PhotoService.GroupPhotosAsync(photos);
+        
+        foreach (var group in groupedPhotos)
+        {
+            var photoGroup = new PhotoGroup(group);
+            
+            foreach (var photo in photoGroup.Photos)
+            {
+                Images.Add(photo);
+            }
+        }
     }
 }
