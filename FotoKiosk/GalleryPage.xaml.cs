cb20d16747a84bf625737dfebcf2731796be5aae
nervot94 <236539115+nervot94@users.noreply.github.com>
2026-06-03 22:14:56 +0200

    Update selected photo count text when selecting images.

--- FotoKiosk/GalleryPage.xaml.cs
+++ FotoKiosk/GalleryPage.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,10 +25,27 @@ namespace FotoKiosk;
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class GalleryPage : Page
+public sealed partial class GalleryPage : Page, INotifyPropertyChanged
 {
     public ObservableCollection<RollercoasterPhoto> Images { get; } = [];
     private LocalPhotoService PhotoService { get; } = new();
+
+    private int _selectedCount;
+    public int SelectedCount
+    {
+        get => _selectedCount;
+        set
+        {
+            if (_selectedCount == value) return;
+            _selectedCount = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCount)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCountText)));
+        }
+    }
+
+    public string SelectedCountText => $"Aantal fotos geselecteerd: {SelectedCount}";
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     public GalleryPage()
     {
@@ -52,5 +70,10 @@ public sealed partial class GalleryPage : Page
                 Images.Add(photo);
             }
         }
+    }
+
+    private void ImageGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        SelectedCount = ImageGridView.SelectedItems.Count;
     }
 }
