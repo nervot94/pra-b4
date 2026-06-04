c0c93e74a1752d719d8684feb4ab8673759129b4
nervot94 <236539115+nervot94@users.noreply.github.com>
2026-06-04 09:42:31 +0200

    Remove unused fields on Rollercoaster photo related classes

--- FotoKiosk/Models/PhotoGroup.cs
+++ FotoKiosk/Models/PhotoGroup.cs
@@ -6,8 +6,6 @@ public class PhotoGroup
 {
     public ObservableCollection<RollercoasterPhoto> Photos { get; set; } = [];
     
-    public bool IsSelected { get; set; } = false;
-
     public decimal Price { get; set; } = 5.0m;
     
     public TimeSpan DateTaken => Photos.FirstOrDefault()?.Metadata.DateTaken ?? TimeSpan.Zero;
@@ -20,31 +18,5 @@ public class PhotoGroup
         {
             Photos.Add(photo);
         }
-    }
-    
-    public void ToggleSelection()
-    {
-        if (IsSelected)
-            DeselectAll();
-        else
-            SelectAll();
-    }
-    
-    private void SelectAll()
-    {
-        foreach (var photo in Photos)
-        {
-            photo.IsSelected = true;
-        }
-        IsSelected = true;
-    }
-    
-    private void DeselectAll()
-    {
-        foreach (var photo in Photos)
-        {
-            photo.IsSelected = false;
-        }
-        IsSelected = false;
     }
 }
--- FotoKiosk/Models/RollercoasterPhoto.cs
+++ FotoKiosk/Models/RollercoasterPhoto.cs
@@ -4,7 +4,4 @@ public class RollercoasterPhoto
 {
     public string FilePath { get; set; }
     public RollercoasterPhotoMetadata Metadata { get; set; }
-
-    public bool IsSelected { get; set; } = false;
-    
 }
