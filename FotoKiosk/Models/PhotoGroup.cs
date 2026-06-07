136c4bc30f89c554a02acc0a746d6042e16e632e
nervot94 <236539115+nervot94@users.noreply.github.com>
2026-06-07 20:10:34 +0200

    Remove useless fields on PhotoGroup class

--- FotoKiosk/Models/PhotoGroup.cs
+++ FotoKiosk/Models/PhotoGroup.cs
@@ -5,12 +5,6 @@ namespace FotoKiosk.Models;
 public class PhotoGroup
 {
     public ObservableCollection<RollercoasterPhoto> Photos { get; set; } = [];
-    
-    public decimal Price { get; set; } = 5.0m;
-    
-    public TimeSpan DateTaken => Photos.FirstOrDefault()?.Metadata.DateTaken ?? TimeSpan.Zero;
-    public string FormattedTime => DateTaken.ToString(@"hh\:mm\:ss");
-    public string FormattedPrice => $"€{Price:F0}";
 
     public PhotoGroup(RollercoasterPhoto[] photos)
     {
